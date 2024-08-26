a5539441833e305e5e41c5727397a8879a2c87e4
Vedansh1857 <vedanshgupta606@gmail.com>
2024-08-26 21:47:20 +0530

    procfile added

--- myproject/settings.py
+++ myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z)j^ed*ue&6x%vcebu9+!vxtvy+#&=1i0*a=x(!3gmhk0)ksd^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
